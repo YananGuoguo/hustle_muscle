0d712710c69c06ce8aa33844fcdd716e1c185514
yananguoguo <yananguoguo@gmail.com>
2022-05-08 00:35:02 -0400

    [ORGANISE] Home Page Title finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hustle_muscle/home_page.dart';
+import 'colors.dart' as color;
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,54 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: Scaffold(
+          backgroundColor: color.AppColor.homePageBackground,
+          body: Container(
+              padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("Training",
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: color.AppColor.homePageTitle,
+                              fontWeight: FontWeight.w700)),
+                      Expanded(child: Container()),
+                      Icon(Icons.arrow_back_ios_new_outlined,
+                          size: 20, color: color.AppColor.homePageIcons),
+                      const SizedBox(width: 10),
+                      Icon(Icons.calendar_today_outlined,
+                          size: 20, color: color.AppColor.homePageIcons),
+                      const SizedBox(width: 10),
+                      Icon(Icons.arrow_forward_ios_outlined,
+                          size: 20, color: color.AppColor.homePageIcons)
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Text("Your Program",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: color.AppColor.homePageSubtitle,
+                              fontWeight: FontWeight.w600)),
+                      Expanded(child: Container()),
+                      Text("Details",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: color.AppColor.homePageDetail,
+                              fontWeight: FontWeight.w600)),
+                      const SizedBox(width: 5),
+                      Icon(Icons.arrow_forward_ios_outlined,
+                          size: 18, color: color.AppColor.homePageIcons)
+                    ],
+                  ),
+                  Container()
+                ],
+              ))),
     );
   }
 }
