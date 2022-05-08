1d6e8e4bbc101d3d8e993d0396f8ee90f6943b65
yananguoguo <yananguoguo@gmail.com>
2022-05-08 00:45:18 -0400

    [ORGANISE] Transferred home_page widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'colors.dart' as color;
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -10,6 +12,58 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: color.AppColor.homePageBackground,
+        body: Container(
+            padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text("Training",
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: color.AppColor.homePageTitle,
+                            fontWeight: FontWeight.w700)),
+                    Expanded(child: Container()),
+                    Icon(Icons.arrow_back_ios_new_outlined,
+                        size: 20, color: color.AppColor.homePageIcons),
+                    const SizedBox(width: 10),
+                    Icon(Icons.calendar_today_outlined,
+                        size: 20, color: color.AppColor.homePageIcons),
+                    const SizedBox(width: 10),
+                    Icon(Icons.arrow_forward_ios_outlined,
+                        size: 20, color: color.AppColor.homePageIcons)
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Text("Your Program",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: color.AppColor.homePageSubtitle,
+                            fontWeight: FontWeight.w600)),
+                    Expanded(child: Container()),
+                    Text("Details",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: color.AppColor.homePageDetail,
+                            fontWeight: FontWeight.w600)),
+                    const SizedBox(width: 5),
+                    Icon(Icons.arrow_forward_ios_outlined,
+                        size: 18, color: color.AppColor.homePageIcons)
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  color: Colors.purple,
+                )
+              ],
+            )));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hustle Muscle',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,54 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          backgroundColor: color.AppColor.homePageBackground,
-          body: Container(
-              padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Text("Training",
-                          style: TextStyle(
-                              fontSize: 30,
-                              color: color.AppColor.homePageTitle,
-                              fontWeight: FontWeight.w700)),
-                      Expanded(child: Container()),
-                      Icon(Icons.arrow_back_ios_new_outlined,
-                          size: 20, color: color.AppColor.homePageIcons),
-                      const SizedBox(width: 10),
-                      Icon(Icons.calendar_today_outlined,
-                          size: 20, color: color.AppColor.homePageIcons),
-                      const SizedBox(width: 10),
-                      Icon(Icons.arrow_forward_ios_outlined,
-                          size: 20, color: color.AppColor.homePageIcons)
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: [
-                      Text("Your Program",
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: color.AppColor.homePageSubtitle,
-                              fontWeight: FontWeight.w600)),
-                      Expanded(child: Container()),
-                      Text("Details",
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: color.AppColor.homePageDetail,
-                              fontWeight: FontWeight.w600)),
-                      const SizedBox(width: 5),
-                      Icon(Icons.arrow_forward_ios_outlined,
-                          size: 18, color: color.AppColor.homePageIcons)
-                    ],
-                  ),
-                  Container()
-                ],
-              ))),
+      home: HomePage()
     );
   }
 }
