ae8ce3c0583fd3523936a8b09aff55a79e4ea286
yananguoguo <yananguoguo@gmail.com>
2022-05-09 23:00:22 -0400

    video list build

--- lib/video_info.dart
+++ lib/video_info.dart
@@ -13,13 +13,13 @@ class VideoInfo extends StatefulWidget {
 }
 
 class _VideoInfoState extends State<VideoInfo> {
-  List info = [];
+  List videoInfo = [];
 
-  _initData() {
-    DefaultAssetBundle.of(context)
+  _initData() async {
+    await DefaultAssetBundle.of(context)
         .loadString("json/videoinfo.json")
         .then((value) {
-      info = json.decode(value);
+      videoInfo = json.decode(value);
     });
   }
 
@@ -179,7 +179,43 @@ class _VideoInfoState extends State<VideoInfo> {
                                 ),
                                 const SizedBox(width: 30),
                               ],
-                            )
+                            ),
+                            Expanded(
+                                child: ListView.builder(
+                                    itemCount: videoInfo.length,
+                                    itemBuilder: (context, int index) {
+                                      return GestureDetector(
+                                        onTap: () {
+                                          debugPrint(index.toString());
+                                        },
+                                        child: Container(
+                                            height: 120,
+                                            color: Colors.redAccent,
+                                            width: 200,
+                                            child: Column(
+                                              children: [
+                                                Row(
+                                                  children: [
+                                                    Container(
+                                                        width: 80,
+                                                        height: 80,
+                                                        decoration: BoxDecoration(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        10),
+                                                            image: DecorationImage(
+                                                                image: AssetImage(
+                                                                    videoInfo[
+                                                                            index]
+                                                                        [
+                                                                        "thumbnail"]))))
+                                                  ],
+                                                )
+                                              ],
+                                            )),
+                                      );
+                                    }))
                           ],
                         )))
               ],
